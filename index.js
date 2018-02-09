ede8b05c3f396a8173edd3286f332d7d52d631a9
Dan Murphy <dgem@users.noreply.github.com>
2018-02-09 14:28:51 +0000

    add in mongoclient

--- index.js
+++ index.js
@@ -1,19 +1,53 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config();
-console.log("qwe3", process.env.STRIPE_KEY);
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+var MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 const app = express();
+const stripePrivateKey = process.env.STRIPE_KEY
+const stripePublicKey = process.env.STRIPE_PUB
+const mongoURI = process.env.MONGODB_URI
 
-app.use(express.static('public'));
+if (!stripePrivateKey) {
+  console.log("STRIPE_KEY not set")
+}
+if (!stripePublicKey) {
+  console.log("STRIPE_PUB not set")
+}
+if (!mongoURI) {
+  console.log("MONGODB_URI not set")
+}
+
+const stripe = require("stripe")(stripePrivateKey);
+
+
+const db = MongoClient.connect(mongoURI)
+  .then(function(db) {
+    console.log("Connected to mongodb")
+    return db;
+  })
+  .catch(function(err) {
+    console.log("Unable to connection to mongodb")
+  }
+);
 
 app.use(bodyParser.urlencoded({
   extended: false
-}));
+}))
+
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
+
+
+
+app.get('/key', (req, res) => {
+  send(stripePublicKey, 200)
+})
+
 app.post("/charge", (req, res) => {
+
   let amount = 500;
   console.log("req.body", JSON.stringify(req.body))
   stripe.customers.create({
@@ -44,4 +78,14 @@ app.post("/charge", (req, res) => {
     });
 });
 
+
+
 app.listen(process.env.PORT, () => console.log('Farewell Zee listening on', process.env.PORT))
+
+process.on('SIGTERM', () => {
+  app.close().then( () => {
+    db.close().then(() => {
+      process.exit(0)
+    });
+  });
+});
